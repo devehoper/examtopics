550220373d173ad0634d3feaac2ac3bb0b20cb50
devehoper <devehoper@gmail.com>
2022-01-12 14:50:36 +0000

    added css file

--- src/js/exametopics.js
+++ src/js/exametopics.js
@@ -5,7 +5,7 @@ removeCookie("csrftoken");
 
 let txtScore = localStorage.getItem("study_score") != null ? localStorage.getItem("study_score") : 0;
 $("#score").length === 0
-? $("body").prepend("<div class='study_score' style='position:fixed; z-index:99999; border-radius:10px; z-index: 99; left: 10px;top:200px; background-color:#CCC;'><a style='margin-left: 10px;'>Score:</a><span id='score'>" + txtScore + "</span> <p> <button id='resetScore' style='margin-left: 10px; margin-right: 10px' class='btn btn-primary'>Reset</button></div>")
+? $("body").prepend("<div class='study_score' ><a style='margin-left: 10px;'>Score:</a><span id='score'>" + txtScore + "</span> <p> <button id='resetScore' class='btn btn-primary'>Reset</button></div>")
     : null;
 
 $(".multi-choice-item").css("cursor", "pointer");
@@ -14,28 +14,28 @@ $(".multi-choice-item").click(function(e) {
     e.preventDefault();
     let score = 0;
 
-        if(localStorage.getItem("study_score") === null) {
-            localStorage.setItem("study_score", score);
-        } else {
-            score = localStorage.getItem("study_score");
-        }
+    if(localStorage.getItem("study_score") === null) {
+        localStorage.setItem("study_score", score);
+    } else {
+        score = localStorage.getItem("study_score");
+    }
 
-        if($(this).hasClass("correct-hidden")) {
-            if($(this).parent().find(".clicked").length == 0
-            ) {
-                $(this).css("border", "2px solid GREEN");
-                score = Number(score) + Number(1)
-                $(this).addClass("clicked");
-            }
+    if($(this).hasClass("correct-hidden")) {
+        if($(this).parent().find(".clicked").length == 0
+        ) {
             $(this).css("border", "2px solid GREEN");
-            $(this).css("border-radius", "10px");
-            localStorage.setItem("study_score", score);
-            $("#score").html(score);
-        } else {
-            $(this).css("border", "2px solid RED");
-            $(this).css("border-radius", "10px");
+            score = Number(score) + Number(1)
             $(this).addClass("clicked");
         }
+        $(this).css("border", "2px solid GREEN");
+        $(this).css("border-radius", "10px");
+        localStorage.setItem("study_score", score);
+        $("#score").html(score);
+    } else {
+        $(this).css("border", "2px solid RED");
+        $(this).css("border-radius", "10px");
+        $(this).addClass("clicked");
+    }
 });
 
 $("#resetScore").click(function(e) {
