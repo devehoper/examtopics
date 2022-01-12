ce37c433e85274a7c7bab196479ff2e5d9f3ca1e
devehoper <devehoper@gmail.com>
2022-01-12 13:17:15 +0000

    Now no button needed to start the extention

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,10 @@
 let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  chrome.tabs.executeScript(null,{file:"exametopics.js"});
 });
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,30 +1,27 @@
+console.log("POPUPJS");
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['jquery3.6.js', 'exametopics.js'],
-    });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.log("teste");
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor
   });
+});
   
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    console.log("teste");
-    chrome.scripting.executeScript({
-      function: init,
-    });
-    // chrome.storage.sync.get("color", ({ color }) => {
-    //   //document.body.style.backgroundColor = color;
-    // });
-  }
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  
+}
 
 
-  function init() {
-    $("#score").length === 0
-      ? $("body").prepend("<div class='study_score' style='position:fixed; border-radius:10px; z-index: 99; left: 10px;top:200px; background-color:#CCC;'><a style='margin-left: 10px;'>Score:</a><span id='score'>0</span> <p> <button id='resetScore' style='margin-left: 10px; margin-right: 10px' class='btn btn-primary'>Reset</button></div>")
-      : null;
+function init() {
+  $("#score").length === 0
+    ? $("body").prepend("<div class='study_score' style='position:fixed; border-radius:10px; z-index: 99; left: 10px;top:200px; background-color:#CCC;'><a style='margin-left: 10px;'>Score:</a><span id='score'>0</span> <p> <button id='resetScore' style='margin-left: 10px; margin-right: 10px' class='btn btn-primary'>Reset</button></div>")
+    : null;
+
+  $(".multi-choice-item").css("cursor", "pointer");
 
   $(".multi-choice-item").click(function(e) {
     e.preventDefault();
